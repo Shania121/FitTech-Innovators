f54a7589a48b853c4cb800ef70d34cecff604101
Shania121 <125470070+Shania121@users.noreply.github.com>
2024-04-21 22:36:31 +0000

    Move Models

--- App/models.py
+++ App/models.py
@@ -1,4 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
 from sqlalchemy.sql.expression import func
 from werkzeug.security import check_password_hash, generate_password_hash
 
